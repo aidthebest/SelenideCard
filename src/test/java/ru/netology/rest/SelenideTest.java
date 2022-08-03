cf4752bd0cc34e3351c73bf129afd2bba679bef1
Alexander Mizyukin <a.mizyukin@mail.ru>
2022-08-03 15:43:32 +0300

    init

--- src/test/java/ru/netology/rest/SelenideTest.java
+++ src/test/java/ru/netology/rest/SelenideTest.java
@@ -1,6 +1,7 @@
 package ru.netology.rest;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.time.Duration;
@@ -30,22 +31,33 @@ public class SelenideTest {
     }
 
     @Test
-    void test1(){
-
-        Configuration.holdBrowserOpen = true;
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
-        System.setProperty("selenide.browser", "Chrome");
-        open("http://localhost:9999");
-        $x("//input[@placeholder='Город']").val("Самара");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").val(data);
-        $x("//input[@name='name']").val("Иванов Федор");
-        $x("//input[@name='phone']").val("+79375286987");
-        $x("//span[@class='checkbox__box']").click();
-        $x("//span[@class='button__text']").click();
-        $x("//*[contains(text(), 'Забронировать')]").shouldBe(visible, Duration.ofSeconds(15));
-        $(byText("Успешно"));
-        $(byText("Встреча успешно забронирована на "+ data));
-
+    public void shouldSubmitRequest() {
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Петров Василий Винниаминович");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+//        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
+
+//    @Test
+//    void test1(){
+//
+//        Configuration.holdBrowserOpen = true;
+//        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+//        System.setProperty("selenide.browser", "Chrome");
+//        open("http://localhost:9999");
+//        $x("//input[@placeholder='Город']").val("Самара");
+//        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+//        $("[data-test-id='date'] input").val(data);
+//        $x("//input[@name='name']").val("Иванов Федор");
+//        $x("//input[@name='phone']").val("+79375286987");
+//        $x("//span[@class='checkbox__box']").click();
+//        $x("//span[@class='button__text']").click();
+//        $x("//*[contains(text(), 'Забронировать')]").shouldBe(visible, Duration.ofSeconds(15));
+//        $(byText("Успешно"));
+//        $(byText("Встреча успешно забронирована на "+ data));
+//
+//    }
 }
